e43b909d47ca1c19064458d911cbcf42ce7f1d29
Piero Ottuzzi <piero.ottuzzi@brucalipto.org>
2011-10-21 17:02:26 +0200

    Updated

--- sqlUtil/src/org/brucalipto/sqlutil/SQLUtilTypes.java
+++ sqlUtil/src/org/brucalipto/sqlutil/SQLUtilTypes.java
@@ -16,7 +16,6 @@
 package org.brucalipto.sqlutil;
 
 import java.lang.reflect.Field;
-import java.sql.Types;
 import java.util.Collections;
 import java.util.Hashtable;
 import java.util.Map;
@@ -25,7 +24,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * Class rappresenting a useful representation of java.sql.Types
+ * This class is a superset of java.sql.Types containing constants from
+ * others providers (eg. oracle.jdbc.driver.OracleTypes)
  * @author Piero Ottuzzi <piero.ottuzzi@brucalipto.org>
  */
 public class SQLUtilTypes
@@ -34,12 +34,57 @@ public class SQLUtilTypes
 	
 	public final static Map SQL_TYPES = Collections.unmodifiableMap(fillJavaSQLTypes());
 	
+	/* java.sql.Types Constants */
+	public final static int BOOLEAN = 16;
+	public final static int DATALINK = 70;
+	public final static int REF = 2006;
+	public final static int CLOB = 2005;
+	public final static int BLOB = 2004;
+	public final static int ARRAY = 2003;
+	public final static int STRUCT = 2002;
+	public final static int DISTINCT = 2001;
+	public final static int JAVA_OBJECT = 2000;
+	public final static int OTHER = 1111;
+	public final static int NULL = 0;
+	public final static int LONGVARBINARY = -4;
+	public final static int VARBINARY = -3;
+	public final static int BINARY = -2;
+	public final static int TIMESTAMP = 93;
+	public final static int TIME = 92;
+	public final static int DATE = 91;
+	public final static int LONGVARCHAR = -1;
+	public final static int VARCHAR = 12;
+	public final static int CHAR = 1;
+	public final static int DECIMAL = 3;
+	public final static int NUMERIC = 2;
+	public final static int DOUBLE = 8;
+	public final static int REAL = 7;
+	public final static int FLOAT = 6;
+	public final static int BIGINT = -5;
+	public final static int INTEGER = 4;
+	public final static int SMALLINT = 5;
+	public final static int TINYINT = -6;
+	
+	/* oracle.jdbc.driver.OracleTypes Constants */
+	public final static int FIXED_CHAR = 999;
+	public final static int PLSQL_INDEX_TABLE = -14;
+	public final static int JAVA_STRUCT = 2008;
+	public final static int OPAQUE = 2007;
+	public final static int BFILE = -13;
+	public final static int CURSOR = -10;
+	public final static int ROWID = -8;
+	public final static int INTERVALDS = -104;
+	public final static int INTERVALYM = -103;
+	public final static int TIMESTAMPLTZ = -102;
+	public final static int TIMESTAMPTZ = -101;
+	public final static int TIMESTAMPNS = -100;
+	
 	private final static Map fillJavaSQLTypes()
     {
 		Map sqlTypes = new Hashtable();
         try
         {
-            Field[] sqlTypeFields = Types.class.getFields();
+            Field[] sqlTypeFields = SQLUtilTypes.class.getFields();
             if (sqlTypeFields==null)
             {
                 sqlTypeFields = new Field[0];
@@ -48,14 +93,17 @@ public class SQLUtilTypes
             {
                 final Field sqlType = sqlTypeFields[i];
                 final String sqlTypeName = sqlType.getName();
-                final int value = sqlType.getInt(sqlTypeName);
-                log.debug("TypeName: '"+sqlTypeName+"'; TypeValue: '"+value+"'");
-                sqlTypes.put(Integer.valueOf(""+value), sqlTypeName);
+                if (!sqlTypeName.equals("SQL_TYPES"))
+                {
+	                final int value = sqlType.getInt(sqlTypeName);
+	                log.debug("TypeName: '"+sqlTypeName+"'; TypeValue: '"+value+"'");
+	                sqlTypes.put(Integer.valueOf(""+value), sqlTypeName);
+                }
             }
         }
         catch (Exception e)
         {
-            log.error("Error adding java.sql.Types to SQLTypes map");
+            log.error("Error adding java.sql.Types to SQLTypes map", e);
         }
         return sqlTypes;
     }
